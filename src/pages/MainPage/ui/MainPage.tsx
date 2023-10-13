7a50c03267c0dacd646f8dba5715765d736c0427
Dmitriy Vachugov <mitpal16@gmail.com>
2023-10-13 14:43:51 +0400

    add PageLoader, NotFoundPage, ErrorBoundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
 import { useTranslation } from 'react-i18next';
+import { BugButton } from "@/app/providers/ErrorBoundary";
 
 const MainPage = () => {
   const { t } = useTranslation();
 
   return (
     <div>
-      { t('Главная страница') }
-      { t('страница') }
+      <BugButton/>
+      { t( 'Главная страница' ) }
     </div>
   );
 };
